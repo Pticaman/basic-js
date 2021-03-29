9ab43314d4c3d27ee5c0ceae71b45fcc016096ed
Pticaman <socolroman@gmail.com>
2021-03-29 21:52:01 +0300

    fix: fix coun-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let arr = [];
+  let conc = arr.concat(...backyard);
+  let sum = 0;
+  for (let i = 0; i < conc.length; i++) {
+    conc[i] === '^^' ? sum++ : sum;
+  }
+  return sum;
 };
