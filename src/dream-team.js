e48e833789788547807efaecdd34646b4382fca9
Pticaman <socolroman@gmail.com>
2021-03-29 22:23:20 +0300

    fix: fix dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members) || members === undefined) {
+    return false
+  }
+  let filt = members.filter(function (v) {
+    return typeof v === 'string';
+  });
+  let m = filt.map(function (v) {
+    return v.trim()[0].toUpperCase();
+  });
+    return m.sort().join('')
 };
